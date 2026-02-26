ac77c80127e3989bbe1621c64d3873ac1871f554
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-02-26 15:39:28 +0530

    Update stockBuyAndSell.java

--- Arrays/stockBuyAndSell.java
+++ Arrays/stockBuyAndSell.java
@@ -1,6 +1,19 @@
 package Arrays;
 
 public class stockBuyAndSell {
-    
+    static int maxProfit(int[] arr){
+      int profit = 0;
+      for(int i =1;i<arr.length;i++){
+        if(arr[i]>arr[i-1]){
+            profit += arr[i]-arr[i-1];
+        }
+      }
+      return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {1,5,3,8,12};
+        System.out.println(maxProfit(prices));
+    }
     
 }
