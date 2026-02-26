b68059e49656a39312c03961584e41770df2c75a
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-02-26 14:55:29 +0530

    LeetCode

---
+++ LeetCode/217.ContainsDuplicate/Solution.java
@@ -0,0 +1,17 @@
+// package LeetCode.217.ContainsDuplicate;
+
+import java.util.HashSet;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            } else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
