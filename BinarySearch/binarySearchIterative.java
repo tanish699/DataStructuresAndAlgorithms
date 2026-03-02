ec4f90bc2129b94375b4a09710d46a7d6202beb9
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-03-02 14:27:41 +0530

    Create binarySearchIterative.java

---
+++ BinarySearch/binarySearchIterative.java
@@ -0,0 +1,41 @@
+package BinarySearch;
+
+public class binarySearchIterative {
+    public static int binarySearch(int[] arr, int target){
+        int start =0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>target){
+                end = mid - 1;
+            } else if(arr[mid]<target){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+     
+    public static int binarySearch(int[] arr, int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>target){
+                end = mid - 1;
+            } else if(arr[mid]<target){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
