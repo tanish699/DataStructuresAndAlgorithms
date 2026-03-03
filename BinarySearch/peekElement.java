4182729870825eaa1beecd8b909ca2ae3af353cc
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-03-03 10:56:20 +0530

    Update peekElement.java

--- BinarySearch/peekElement.java
+++ BinarySearch/peekElement.java
@@ -5,11 +5,31 @@ public class peekElement {
     Give a un Sorted array
     Peek element is an element which is not Smaller then its neighbours
     */
-    static boolean isPeek(int[] arr,int index){
-        if(arr[index])
+    public static boolean isPeek(int[] arr,int index){
+        return ((index==0)||(arr[index]>=arr[index-1]))&&((index==arr.length-1)||(arr[index]>=arr[index+1]));
     }
-    static int findPeekElement(int[] arr){
 
+    static int findPeekElement(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+       while(start<=end){
+            int mid = start + (end-start) /2;
+            if(isPeek(arr, mid)){
+             return mid;
+            }
+            if(mid>0&&arr[mid-1]>=arr[mid]){
+               end = mid - 1;
+            } else {
+              start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,20,40,30,20,50,60};
+        int peekElement = (findPeekElement(arr));
+        System.out.println("Index : "+ peekElement +", Value : " + arr[peekElement]);
     }
     
 }
