cda2940299cf27748258092e94d3e978c1d1b506
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-03-03 11:08:55 +0530

    Create twoPointerApproach.java

---
+++ BinarySearch/twoPointerApproach.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class twoPointerApproach {
+    // Given a sorted array and a num k find a pair with sum == k;
+    static boolean findSum(int[] arr, int k){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int sum = arr[start]+arr[end];
+            if(sum>k){
+                end--;
+            } else if(sum<k){
+                start++;
+            }else if(sum==k){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,8,12,30};
+        System.out.println(findSum(arr, 17));
+    }
+}
