7089bdde49123717828ade107d161af9c45204c8
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-03-03 10:30:50 +0530

    Create peekElement.java

---
+++ BinarySearch/peekElement.java
@@ -0,0 +1,15 @@
+package BinarySearch;
+
+public class peekElement {
+    /*
+    Give a un Sorted array
+    Peek element is an element which is not Smaller then its neighbours
+    */
+    static boolean isPeek(int[] arr,int index){
+        if(arr[index])
+    }
+    static int findPeekElement(int[] arr){
+
+    }
+    
+}
