27aa8179d9b229038fcba2b5ec0bc3ca76788744
Tanish Wadhwa <tanishwadhwa2310@gmail.com>
2026-03-10 14:11:12 +0530

    Create printDLL.java

---
+++ DoublyLinkedList/printDLL.java
@@ -0,0 +1,19 @@
+package DoublyLinkedList;
+
+public class printDLL {
+
+    public static void print(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value);
+            if(temp.next!=null){
+                System.out.print(" <-> ");
+            }
+            
+            temp = temp.next;
+        }
+        System.out.println();
+        
+    }
+    
+}
